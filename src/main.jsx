4f32f464dd3d4072816ebe481849044854a2a211
yogita-s-24 <yogitashete24@gmail.com>
2023-11-06 05:55:37 +0530

    routing setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import Home from "./views/Home/Home.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
