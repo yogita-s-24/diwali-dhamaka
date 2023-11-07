18fe4b567071373cb8061ffae2531f464776c1be
yogita-s-24 <yogitashete24@gmail.com>
2023-11-07 06:43:49 +0530

    ref : custom css and images added

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,6 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-  },
+  }
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import './Home.css'
 import { useSearchParams } from "react-router-dom";
+import Diya from './diva.png'
+import DiwaliLamp from "./diwali-lamp.png";
+import Lamp from "./lamp.png";
 
 const GREETINGS = [
     "सर्व मित्र परिवाराला …दीपावलीच्या धनदायी ,प्रकाशमय,चैतन्यदायी मंगलमय शुभेच्छा !!!दीपावलीच्या हार्दिक शुभेछा…",
@@ -9,36 +12,99 @@ const GREETINGS = [
     "एक दिवा लावु जिजाऊचरणी।  एक दिवा लावु शिवचरणी।  एक दिवा लावु शंभुचरणी। आमचा इतिहास हीच आमची प्रतिष्ठा….. दिपावलीच्या हार्दिक शिवशुभेच्छा…. आपल्या घरि सुख समाधान सदैव नांदो हिच जगदंबेचरणी प्रार्थना॥ ।। जय शिवराय ।। तुम्हाला व तुमच्या कुटुंबियांना दिवाळीच्या हार्दिक शुभेछा !!",
     "सगळा आनंद सगळे सौख्य,सगळ्या स्वप्नांची पूर्णता,यशाची सगळी शिखरे,सगळे ऐश्वर्य,हे आपल्याला मिळू दे,ही दीपावली आपल्या आयुष्याला एक नवा उजाळा देवू दे…",
     "स्नेहाचा सुगंध दरवळला,आनंदाचा सण आला.विनंती आमची परमेश्वराला,सौख्य, समृध्दी लाभो तुम्हाला.दिवाळीच्या हार्दिक शुभेच्छा!",
-  ];
+];
 
 function Home() {
     const [searchParams] = useSearchParams();
-  const to = searchParams.get("to");
-  const from = searchParams.get("from");
-  const greetingNumber =
-    searchParams.get("s") >= GREETINGS.length ? 0 : searchParams.get("s") || 0;
+    const [to, setTo] = useState(searchParams.get("to"));
+    // const to = searchParams.get("to");
 
-  return(
-    <>
-      <div className="container mt-5">
-        <div className="card">
-          <div className="card-body  position-relative">
-            <div className="to fs-6 fw">
-              <p className=""> 💐Dear {to}</p>
-            </div>
+    const [from, setFrom] = useState(searchParams.get("from"))
+    // const from = searchParams.get("from");
 
-            <div className="mt-4 fs-5 text-center">
-              <p>{GREETINGS[greetingNumber]}</p>
-            </div>
+    const [greetingNumber, setGreetingNumber] = useState(searchParams.get("s") >= GREETINGS.length ? 0 : searchParams.get("s") || 0)
+    // const greetingNumber = searchParams.get("s") >= GREETINGS.length ? 0 : searchParams.get("s") || 0;
 
-            <div className="from">
-              <p className="">🙏 शुभेच्छुक {from}</p>
-            </div>
+    const [theme, setTheme] = useState(searchParams.get("t"));
+
+    return (
+        <>
+            <div className="container mt-5">
+                <div className={`card shadow new-card ${`theme-${theme}`}`} >
+                    <div className="card-body  content">
+                    <div className=" mt-5 position-relative ">
+            <img
+              src={DiwaliLamp}
+              alt="diwali-lamp "
+              className="position-absolute left-lamp"
+            />
+            <img src={Lamp} alt="lamp" className="position-absolute lamp" />
+            <img
+              src={DiwaliLamp}
+              alt="diwali-lamp"
+              className="position-absolute right-lamp"
+            />
           </div>
-        </div>
-      </div>
-    </>
-  )
+                        <div className="to fs-6 fw">
+                            <p className=""> 💐Dear {to}</p>
+                        </div>
+
+                     
+
+                        <div className="fs-5 text-center">
+                            <p>{GREETINGS[greetingNumber]}</p>
+                        </div>
+
+                        <div className="from ">
+                            <p className="">🙏 शुभेच्छुक {from}</p>
+                        </div>
+                    </div>
+
+                   
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3">
+                        <input type="text" className="mt-5 input-box" placeholder="To" />
+                    </div>
+
+                    <div className="col-md-3">
+                        <input type="text" className="mt-5 input-box" placeholder="From" />
+                    </div>
+                    <div className="col-md-3">
+                        <select className="mt-5 select-container"
+                            value={greetingNumber}
+                            onChange={(e) => {
+                                setGreetingNumber(e.target.value);
+                            }}>
+                            <option value={0}>Gretting-1</option>
+                            <option value={1}>Gretting-2</option>
+                            <option value={2}>Gretting-3</option>
+                            <option value={3}>Gretting-4</option>
+                            <option value={4}>Gretting-5</option>
+                        </select>
+                    </div>
+
+                    <div className="col-md-3">
+                        <select className="mt-5 select-container"
+                            value={theme}
+                            onChange={(e) => {
+                                setTheme(e.target.value);
+                            }}>
+                            <option value="orange">Orange Theme</option>
+                            <option value="red" className="bg-danger">Red Theme</option>
+                            <option value="purple">Purplr Theme</option>
+                            <option value="blue">Blue Theme</option>
+                            <option value="yellow">Yellow Theme</option>
+                        </select>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
 }
 
 export default Home;
